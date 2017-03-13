9dd26f5c37b1f387555c0236cd82ca7f21cc31c0
John Wiley Quarles <john.w.quarles@gmail.com>
2017-03-13 02:19:31 -0500

    finished section 1.2

--- temp-convert.c
+++ temp-convert.c
@@ -5,15 +5,15 @@
 
 int main(void)
 {
-  int fahr, celsius;
+  float fahr, celsius;
   int lower = 0;
   int upper = 300;
   int step = 20;
 
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr - 32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr += step;
   }
 }
